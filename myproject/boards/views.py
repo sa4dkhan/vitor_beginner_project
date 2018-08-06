faeeaa03031021148dbf7c3a83f1c70094a9242b
Saad Khan <saad.khan@nextdot.com.au>
2018-08-06 16:51:27 +0600

    second commit

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from boards.models import Board
+
+
+# def home(request):
+#     boards = Board.objects.all()
+#     boards_name = list()
+#
+#     for board in boards:
+#         boards_name.append(board.name)
+#
+#     response_html = '<br>'.join(boards_name)
+#
+#     return HttpResponse(response_html)
 
 
 def home(request):
-    return HttpResponse("Hello World")
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
